34d1a46547cd41c1576c03a210a41da39f97efad
p.v.s.fant@gmail.com <Fantella1978>
2023-01-12 20:15:01 +0200

    First commit

---
+++ IDEModuleParser.dpr
@@ -0,0 +1,17 @@
+program IDEModuleParser;
+
+uses
+  Vcl.Forms,
+  UnitMain in 'UnitMain.pas' {Form1},
+  Vcl.Themes,
+  Vcl.Styles;
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Glow');
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ UnitMain.pas
@@ -0,0 +1,63 @@
+unit UnitMain;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, System.Actions, Vcl.ActnList,
+  Vcl.ComCtrls, Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.ExtDlgs, Vcl.Buttons,
+  System.ImageList, Vcl.ImgList;
+
+type
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Panel1: TPanel;
+    TabControl1: TTabControl;
+    ActionList1: TActionList;
+    actExit: TAction;
+    actOpenModuleFile: TAction;
+    OpenTextFileDialog1: TOpenTextFileDialog;
+    Button3: TButton;
+    actParseModuleFile: TAction;
+    bbOpenTextFile: TBitBtn;
+    ImageList1: TImageList;
+    SpeedButton1: TSpeedButton;
+    Image1: TImage;
+    /// <summary>Exit from application</summary>
+    procedure actExitExecute(Sender: TObject);
+    /// <summary>Open Module text file</summary>
+    procedure actOpenModuleFileExecute(Sender: TObject);
+    procedure FormCreate(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.actExitExecute(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TForm1.actOpenModuleFileExecute(Sender: TObject);
+begin
+  //
+
+end;
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  //
+  ImageList1.GetBitmap(0, Image1.Picture.Bitmap);
+
+end;
+
+end.
